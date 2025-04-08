5dd5a4a71d0251ad2e9bc5c35487536402be698f
JrnJ <jeroenjochems@outlook.com>
2025-04-08 11:37:05 +0200

    auth fix

--- AuthenticationService/models/user.js
+++ AuthenticationService/models/user.js
@@ -2,21 +2,32 @@ import mongoose from 'mongoose'
 import crypto from 'crypto';
 
 const userSchema = new mongoose.Schema({
-    isOwner: {type: Boolean},
-    uid: {type: String},
-    email: {type: String, unique: true, required: true},
-    hash: {type: String, required: true},
-    salt: {type: String, required: true}
+    isOwner: { type: Boolean},
+    uid: { type: String},
+    email: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    salt: { type: String, required: true }
 });
 
-userSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
+// This isnt called due to salt and has not being set in the object???
+// userSchema.pre('save', function (next) {
+//     if (this.isModified('password')) {
+//         this.salt = crypto.randomBytes(16).toString('hex');
+//         this.hash = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
+//         this.password = undefined;
+//     }
+//     next();
+// });
+
+userSchema.virtual('password')
+    .set(function(password) {
+        this._password = password; // Optional: store in memory
         this.salt = crypto.randomBytes(16).toString('hex');
-        this.hash = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
-        this.password = undefined;
-    }
-    next();
-});
+        this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    })
+    .get(function() {
+        return this._password;
+    });
 
 const User = mongoose.model('User', userSchema);
 export default User;
