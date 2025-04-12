00f9900ef3e6497c8a8cbd4fe0a007a91fad0e00
JrnJ <jeroenjochems@outlook.com>
2025-04-12 22:18:37 +0200

    all entries

--- score-service/routes/score.js
+++ score-service/routes/score.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { generateScore, joinTarget, getScore } from '../service/service.js';
+import { generateScore, joinTarget, getScore, getAllScore } from '../service/service.js';
 
 const router = express.Router();
 
@@ -28,6 +28,16 @@ router.post('/joinTarget', async (req, res) => {
 router.get('/getScore', async (req, res) => {
     try {
         const response = await getScore(req.body);
+        res.status(200).json({ score: response });
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ message: error.message });
+    }
+});
+
+router.get('/getAllScore', async (req, res) => {
+    try {
+        const response = await getAllScore(req.body);
         res.status(200).json({ score: response });
     } catch (error) {
         console.log(error);
