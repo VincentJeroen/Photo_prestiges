6c6e91218df34163fb46d7ccc7544a1dcd7c30af
VindSent <vincentvanhintum@gmail.com>
2025-04-13 11:34:53 +0200

    fyfcfh

--- register-service/app.js
+++ register-service/app.js
@@ -3,12 +3,17 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import registerRoutes from './routes/register.routes.js';
 import {handleMessages} from "./service/consumer.js";
+import promBundle from 'express-prom-bundle';
 
 // Database
 mongoose.connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`);
 
+// Monitoring
+const metricsMiddleware = promBundle({ includeMethod: true });
+
 const app = express();
 app.use(express.json());
+app.use(metricsMiddleware);
 
 // Routes
 app.use('/', registerRoutes);
