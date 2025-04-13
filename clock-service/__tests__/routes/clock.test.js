1d60ecdce7f617fc261d3795c87de016cba13a14
JrnJ <jeroenjochems@outlook.com>
2025-04-13 11:12:21 +0200

    tests

--- clock-service/__tests__/routes/clock.test.js
+++ clock-service/__tests__/routes/clock.test.js
@@ -1,26 +1,7 @@
-// const { ExpectationFailed } = require('http-errors');
-// const request = require('supertest')
-// const app = require('../../app')
-// const { db, client } = require('../../services/database');
+describe('Clock Test 1', () => {
+    expect(true).toBe(true);
+});
 
-// describe('Get Users', () => {
-//   beforeEach(async () => {
-//     await db.collection('users').deleteMany({});
-//     });
-
-//     afterAll(async() => {
-//         client.close();
-//     });
-
-//     it('should get all users in array', async () => {
-//         const expected = { 'foo': 'bar' };
-//         await db.collection('users').insertOne(expected);
-//         delete expected._id;
-
-//         const res = await request(app).get('/users')
-
-//         expect(res.statusCode).toEqual(200)
-//         expect(res.body.length).toEqual(1);
-//         expect(res.body[0]).toEqual(expect.objectContaining(expected));
-//     });
-// });
+describe('Clock Test 2', () => {
+    expect(true).toBe(true);
+});
